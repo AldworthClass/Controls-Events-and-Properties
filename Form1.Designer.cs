13ae161505c0123c40c4561aa194a7fb85996c6e
AldworthClass <46466802+AldworthClass@users.noreply.github.com>
2021-01-18 14:02:20 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Controls__Events_and_Properties
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.imgCharacter = new System.Windows.Forms.PictureBox();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.btnRandomColor = new System.Windows.Forms.Button();
+            this.btnYellow = new System.Windows.Forms.Button();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.btnRed = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgCharacter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(18, 43);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(233, 34);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Click Me";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // imgCharacter
+            // 
+            this.imgCharacter.Image = ((System.Drawing.Image)(resources.GetObject("imgCharacter.Image")));
+            this.imgCharacter.Location = new System.Drawing.Point(18, 91);
+            this.imgCharacter.Name = "imgCharacter";
+            this.imgCharacter.Size = new System.Drawing.Size(233, 234);
+            this.imgCharacter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgCharacter.TabIndex = 1;
+            this.imgCharacter.TabStop = false;
+            this.imgCharacter.Visible = false;
+            this.imgCharacter.Click += new System.EventHandler(this.imgCharacter_Click);
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(12, 9);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(456, 31);
+            this.lblInstructions.TabIndex = 2;
+            this.lblInstructions.Text = "Click on the Button or the PictureBox";
+            this.lblInstructions.MouseEnter += new System.EventHandler(this.lblInstructions_MouseEnter);
+            this.lblInstructions.MouseLeave += new System.EventHandler(this.lblInstructions_MouseLeave);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(257, 48);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(60, 24);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Status";
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Enabled = false;
+            this.btnQuit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuit.Location = new System.Drawing.Point(257, 291);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(206, 34);
+            this.btnQuit.TabIndex = 4;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBlue.Location = new System.Drawing.Point(257, 171);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(206, 34);
+            this.btnBlue.TabIndex = 5;
+            this.btnBlue.Text = "Blue";
+            this.btnBlue.UseVisualStyleBackColor = true;
+            this.btnBlue.Click += new System.EventHandler(this.btnMakeFormBlue_Click);
+            // 
+            // btnRandomColor
+            // 
+            this.btnRandomColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRandomColor.Location = new System.Drawing.Point(257, 251);
+            this.btnRandomColor.Name = "btnRandomColor";
+            this.btnRandomColor.Size = new System.Drawing.Size(206, 34);
+            this.btnRandomColor.TabIndex = 6;
+            this.btnRandomColor.Text = "Random";
+            this.btnRandomColor.UseVisualStyleBackColor = true;
+            this.btnRandomColor.Click += new System.EventHandler(this.btnRandomColor_Click);
+            // 
+            // btnYellow
+            // 
+            this.btnYellow.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnYellow.Location = new System.Drawing.Point(257, 211);
+            this.btnYellow.Name = "btnYellow";
+            this.btnYellow.Size = new System.Drawing.Size(206, 34);
+            this.btnYellow.TabIndex = 7;
+            this.btnYellow.Text = "Yellow";
+            this.btnYellow.UseVisualStyleBackColor = true;
+            this.btnYellow.Click += new System.EventHandler(this.btnYellow_Click);
+            // 
+            // btnGreen
+            // 
+            this.btnGreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGreen.Location = new System.Drawing.Point(257, 131);
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.Size = new System.Drawing.Size(206, 34);
+            this.btnGreen.TabIndex = 8;
+            this.btnGreen.Text = "Green";
+            this.btnGreen.UseVisualStyleBackColor = true;
+            this.btnGreen.Click += new System.EventHandler(this.btnGreen_Click);
+            // 
+            // btnRed
+            // 
+            this.btnRed.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRed.Location = new System.Drawing.Point(257, 91);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(206, 34);
+            this.btnRed.TabIndex = 9;
+            this.btnRed.Text = "Red";
+            this.btnRed.UseVisualStyleBackColor = true;
+            this.btnRed.Click += new System.EventHandler(this.btnRed_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 337);
+            this.Controls.Add(this.btnRed);
+            this.Controls.Add(this.btnGreen);
+            this.Controls.Add(this.btnYellow);
+            this.Controls.Add(this.btnRandomColor);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.imgCharacter);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "FormMain";
+            this.Text = "Controls, Events and Prperties";
+            ((System.ComponentModel.ISupportInitialize)(this.imgCharacter)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.PictureBox imgCharacter;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Button btnRandomColor;
+        private System.Windows.Forms.Button btnYellow;
+        private System.Windows.Forms.Button btnGreen;
+        private System.Windows.Forms.Button btnRed;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Controls__Events_and_Properties
+{
+    public partial class FormMain : Form
+    {
+        Random generator = new Random();  // Used to make random numbers
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            imgCharacter.Visible = true;
+            lblStatus.Text = "Status: Clicked";
+            btnQuit.Enabled = true;
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void lblInstructions_MouseEnter(object sender, EventArgs e)
+        {
+            lblInstructions.ForeColor = Color.Red;
+        }
+
+        private void lblInstructions_MouseLeave(object sender, EventArgs e)
+        {
+            lblInstructions.ForeColor = Color.Black;
+        }
+
+        private void imgCharacter_Click(object sender, EventArgs e)
+        {
+            imgCharacter.Visible = false;
+        }
+
+        private void btnMakeFormBlue_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.LightBlue;
+        }
+
+        private void btnRed_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Red;
+        }
+
+        private void btnGreen_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Green;
+        }
+
+        private void btnYellow_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Yellow;
+        }
+
+        private void btnRandomColor_Click(object sender, EventArgs e)
+        {
+            int max = 257; // 256 is the maximum value an RGB value can be
+            int r = generator.Next(max);
+            int g = generator.Next(max);
+            int b = generator.Next(max);
+            this.BackColor = Color.FromArgb(r, g, b);
+        }
+    }
+}
